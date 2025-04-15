a5e4b3a7c2d2d02233aa54d8efdedf1a4b41272c
YanSCCA <194171014+YanSCCA@users.noreply.github.com>
2025-04-15 08:59:13 -0700

    feat: add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './Home.jsx'
+import { HashRouter, Route, Routes } from "react-router"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )

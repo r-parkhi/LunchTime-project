06a53df0eabdb66ff5aec3b1a221b164f99bdd53
r-parkhi <radhika.pparkhi@gmail.com>
2025-04-16 11:06:48 -0700

    home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div class="menu">
+      <div class="sides">
+        <h3>Sides</h3>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+      </div>
+
+      <div class="entrees">
+        <h3>Entrees</h3>
+        <img src="https://placehold.co/225x110"></img>
+        <img src="https://placehold.co/225x110"></img>
+      </div>
+
+      <div class="drinks">
+        <h3>Drinks</h3>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+        <img src="https://placehold.co/75x75"></img>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Home

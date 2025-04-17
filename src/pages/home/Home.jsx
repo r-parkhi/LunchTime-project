4f4c67f0c4cbc4b9331ac3f9c6d4d7ac1b68ffd8
r-parkhi <radhika.pparkhi@gmail.com>
2025-04-17 09:48:57 -0700

    homepage layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,29 +3,46 @@ import './Home.css'
 function Home() {
   return (
     <>
-    <div class="menu">
-      <div class="sides">
-        <h3>Sides</h3>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
+    <h4>Tue March 11, 2025</h4>
+
+    <div className="menu">
+      <div className="sides">
+        <h2>Sides</h2>
+        <div className="sideItems">
+          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="https://placehold.co/60x60"></img></div>
+        </div>
       </div>
 
-      <div class="entrees">
-        <h3>Entrees</h3>
-        <img src="https://placehold.co/225x110"></img>
-        <img src="https://placehold.co/225x110"></img>
+      <div className="entrees">
+        <h2>Entrees</h2>
+        <div className="entreeItems">
+          <div className="special">
+            <img src="https://placehold.co/225x110"></img>
+            <img src="https://placehold.co/225x110"></img>
+          </div>
+          <div className="daily">
+            <div><img src="https://placehold.co/75x75"></img></div>
+            <div><img src="https://placehold.co/75x75"></img></div>
+            <div><img src="https://placehold.co/75x75"></img></div>
+            <div><img src="https://placehold.co/75x75"></img></div>
+          </div>
+        </div>
       </div>
 
-      <div class="drinks">
-        <h3>Drinks</h3>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
-        <img src="https://placehold.co/75x75"></img>
+      
+      <div className="drinks">
+        <h2>Drinks</h2>
+        <div className="drinkItems">
+          <div><img src="https://placehold.co/75x75"></img></div>
+          <div><img src="https://placehold.co/75x75"></img></div>
+          <div><img src="https://placehold.co/75x75"></img></div>
+          <div><img src="https://placehold.co/75x75"></img></div>
+        </div>
       </div>
     </div>
     </>

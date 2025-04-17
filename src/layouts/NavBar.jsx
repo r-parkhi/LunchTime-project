b21b869ff9475a1410352149deea63149cdf5703
EricNalasco <nalascoe5014@my.sduhsd.net>
2025-04-17 09:01:01 -0700

    Finish nav bar look

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -5,19 +5,19 @@ import Logo from "../assets/LunchTimeLogo.png"
 function NavBar() {
   return (
     <header>
-    <ul>
-        <div className="logoname">
+    <div className="navbar">
+        <ul className="logoname">
             <img src={Logo}></img>
             <h1>LunchTime!</h1>
-        </div>
-        <div className="navbtns">
+        </ul>
+        <ul className="navbtns">
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/calendar">Calendar</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/faq">FAQ</NavLink></li>
             <li className="loginbtn">Login</li>
-        </div>
-    </ul>
+        </ul>
+    </div>
     </header>
   )
 }

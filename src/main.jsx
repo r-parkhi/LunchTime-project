ed57e972ba414c178a1623bd56694109717333a9
YanSCCA <194171014+YanSCCA@users.noreply.github.com>
2025-04-18 08:40:48 -0700

    fix: importing issues with Help page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter, Route, Routes } from "react-router"
 import './index.css'
 import AppLayout from "./layouts/AppLayout.jsx"
-import About from "./pages/About.jsx"
+import About from "./pages/about/About.jsx"
 import Calendar from "./pages/Calendar.jsx"
 import Home from './pages/home/Home.jsx'
-import Help from './pages/Help.jsx'
+import Help from './pages/help/Help.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

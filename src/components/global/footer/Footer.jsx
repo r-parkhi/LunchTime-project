66328f9e26445c5a6622eee58f4818a18952ed01
YanSCCA <194171014+YanSCCA@users.noreply.github.com>
2025-04-18 09:53:47 -0700

    fix: footer position
    TODO: height of body is still weird

---
+++ src/components/global/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+
+function Footer() {
+  return (
+    <footer>
+      <p className="copyRight">&copy; 2025 LunchTime! Team</p>
+    </footer>
+  );
+}
+export default Footer

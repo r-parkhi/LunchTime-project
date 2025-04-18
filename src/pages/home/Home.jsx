2619482c70009c018c0987a9b4632f428a7d1aa9
r-parkhi <radhika.pparkhi@gmail.com>
2025-04-18 09:58:27 -0700

    homepage side and drink images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,12 +9,12 @@ function Home() {
       <div className="sides">
         <h2>Sides</h2>
         <div className="sideItems">
-          <div><img src="https://placehold.co/60x60"></img></div>
-          <div><img src="https://placehold.co/60x60"></img></div>
-          <div><img src="https://placehold.co/60x60"></img></div>
-          <div><img src="https://placehold.co/60x60"></img></div>
-          <div><img src="https://placehold.co/60x60"></img></div>
-          <div><img src="https://placehold.co/60x60"></img></div>
+          <div><img src="src/assets/sides/celery.jpeg"></img></div>
+          <div><img src="src/assets/sides/banana.jpeg"></img></div>
+          <div><img src="src/assets/sides/carrots.webp"></img></div>
+          <div><img src="src/assets/sides/grapes.jpeg"></img></div>
+          <div><img src="src/assets/sides/redapples.webp"></img></div>
+          <div><img src="src/assets/sides/strawberries.jpeg"></img></div>
         </div>
       </div>
 
@@ -38,10 +38,10 @@ function Home() {
       <div className="drinks">
         <h2>Drinks</h2>
         <div className="drinkItems">
-          <div><img src="https://placehold.co/75x75"></img></div>
-          <div><img src="https://placehold.co/75x75"></img></div>
-          <div><img src="https://placehold.co/75x75"></img></div>
-          <div><img src="https://placehold.co/75x75"></img></div>
+          <div><img src="src/assets/drinks/milk.png"></img></div>
+          <div><img src="src/assets/drinks/chocmilk.png"></img></div>
+          <div><img src="src/assets/drinks/orangejuice.jpeg"></img></div>
+          <div><img src="src/assets/drinks/berryjuice.png"></img></div>
         </div>
       </div>
     </div>

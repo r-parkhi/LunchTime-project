9e4c6bd1c2e98f6619e84f3c5a4147d1b42811f1
Timothy <spillanet3406@my.sduhsd.net>
2025-04-28 09:56:24 -0700

    commit

---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,24 @@
+import "./StarRating.css";
+function StarRating(){
+    return(
+       <>
+        <span onclick="gfg(1)"
+                class="star">★
+        </span>
+        <span onclick="gfg(2)"
+                class="star">★
+        </span>
+        <span onclick="gfg(3)"
+                class="star">★
+        </span>
+        <span onclick="gfg(4)"
+                class="star">★
+        </span>
+        <span onclick="gfg(5)"
+                class="star">★
+        </span>
+        </>
+
+    );
+}
+export default StarRating;

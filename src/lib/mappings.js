44c11921022ca11dddcca9488d369d265730430e
Timothy <spillanet3406@my.sduhsd.net>
2025-05-06 08:46:25 -0700

    commit

--- src/lib/mappings.js
+++ src/lib/mappings.js
@@ -18,7 +18,28 @@ export const food = {
     "660310a1e96f1ec2df04a5b4":"/src/assets/Entree-Items/Chipotle-Ranch-Taco-Salad.jpg",
     "670ec9c2e96f1e7cf45ce517":"/src/assets/Entree-Items/TSOChickenRiceBowl.jpg",
     "64ef7ce7e96f1ed3af81b3ae":"/src/assets/Entree-Items/Cheeseburger.jpg"
-}//  cheeseburger
+}
+export const descriptions = {
+    "64ef7ce7e96f1ed3af81b3a1":"/src/assets/Entree-Items/DeliHamSandwich.jpg", 
+    "64ef7ce7e96f1ed3af81b3a0":"/src/assets/Entree-Items/ChickenRiceBowl.jpg",
+    "64ef7ce7e96f1ed3af81b3a2":"/src/assets/Entree-Items/cheesepizza.jpg",
+    "64ef7ce7e96f1ed3af81b3a3":"/src/assets/Entree-Items/PepperoniPizza.jpg",
+    "650b4b30e96f1e0dd8c736eb":"/src/assets/Entree-Items/PBJ.jpg",
+    "64ef7ce7e96f1ed3af81b3a4":"/src/assets/Entree-Items/HummusBistro.jpg",
+    "660310a0e96f1ec2df04a5b3":"/src/assets/Entree-Items/BeefTeriyakiBowl.jpg",
+    "64ef7ce7e96f1ed3af81b3a8":"/src/assets/Entree-Items/caesarsalad.jpg",
+    "67d49325e96f1ee52b0572f4":"/src/assets/Entree-Items/MacNCheese.jpg",
+    "64ef7ce7e96f1ed3af81b3ab":"The crispy chicken sandwich provides a spicy option to the table to those who want it.",
+    "64ef7ce7e96f1ed3af81b3a7":"Pupusa is a flatbread stuffed with beans and cheese, a nutritous yet scrumptious combo.",
+    "670ec9c2e96f1e7cf45ce518":"The Taco Salad brings several flavors to the plate with beef and salad.",
+    "6557b198e96f1e5c905f7d80":"The hotdog is a simple yet nearly perfected form of classic true American food.",
+    "64ef7ce7e96f1ed3af81b3ac":"The Yougurt Parfait provides a nutritous mix of yogurt fruits and granola into a delectable cup.",
+    "65d77202e96f1e86245bf647":"The Chicken Burrito is a simple yet scrumptious meal appealing to any chicken enjoyer.",
+    "6532d0cbe96f1ee4a746b881":"Pulled pork in a sandwich brings a delightful meat into an easy to consume sandwich.",
+    "660310a1e96f1ec2df04a5b4":"The Taco Salad brings several flavors to the plate with beef and salad with the addition of ranch.",
+    "670ec9c2e96f1e7cf45ce517":"The General TSO Chicken and Rice bowl uses a special sauce to coat delicious chicen on top of brown rice.",
+    "64ef7ce7e96f1ed3af81b3ae":"The Cheeseburger is the standard single patty burger slathered with delectable cheese, offering a great lunch to any student."
+}
 export const allergens = {
     "Dairy":"/src/assets/Allergens/DairyAllergy.jpg",
     "Egg":"/src/assets/Allergens/EggAllergy.jpg",

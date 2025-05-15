d8abe1a4ccf60dd255fe829d5ed4df09b52ed098
YanSCCA <194171014+YanSCCA@users.noreply.github.com>
2025-05-15 08:57:14 -0700

    feat: submit feedback to db

--- src/pages/feedback/feedback.jsx
+++ src/pages/feedback/feedback.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import './feedback.css';
+import { auth } from "../../firebase";
+import { addFeedback } from "../../lib/db";
 
 function App() {
   // State to store form input
@@ -22,39 +24,47 @@ function App() {
   // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevent page reload
+
+    const currentUser = auth.currentUser;
+    if (!currentUser) {
+      alert("You must be signed in to provide feedback!")
+      return;
+    }
+
     setSubmittedData(formData); // Save submitted data
+    addFeedback(currentUser.uid, submittedData.name)
   };
 
   return (
     <>
-    <div className="bodyText">
-      <h1>Enter your feedback here!</h1>
-      <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
-        <div style={{ marginBottom: "10px" }}>
-          <label>
-            Message:{" "}
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Enter your feedback"
-              required
-            />
-          </label>
-        </div>
-        <button type="submit" style={{ padding: "5px 10px" }}>
-          Submit
-        </button>
-      </form>
+      <div className="bodyText">
+        <h1>Enter your feedback here!</h1>
+        <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+          <div style={{ marginBottom: "10px" }}>
+            <label>
+              Message:{" "}
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter your feedback"
+                required
+              />
+            </label>
+          </div>
+          <button type="submit" style={{ padding: "5px 10px" }}>
+            Submit
+          </button>
+        </form>
 
-      {submittedData && (
-        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
-          <h2>Submitted Data:</h2>
-          <p><strong>Feedback:</strong> {submittedData.name}</p>
-          <p><strong>Thank you!</strong></p>
-        </div>
-      )}
+        {submittedData && (
+          <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+            <h2>Submitted Data:</h2>
+            <p><strong>Feedback:</strong> {submittedData.name}</p>
+            <p><strong>Thank you!</strong></p>
+          </div>
+        )}
       </div>
     </>
   );

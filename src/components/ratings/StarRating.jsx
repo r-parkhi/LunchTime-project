533efd73b48fd9d127afe4fc4f393204185455af
EricNalasco <nalascoe5014@my.sduhsd.net>
2025-05-16 09:40:34 -0700

    Finally finished star rating average

--- src/components/ratings/StarRating.jsx
+++ src/components/ratings/StarRating.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import "./StarRating.css"
 import { addRating, getUserRating } from "../../lib/db";
 import { auth } from "../../firebase";
+import { collection, query, where, getDocs, getDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase"
+
+
 
 const defaultIcon = "★";
 
@@ -9,6 +13,7 @@ export default function StarRating({ productId, count, icon }) {
   const [rated, setRated] = useState(false);
   const [rating, setRating] = useState(Math.floor(count));
   const [popupVisible, setPopupVisible] = useState(false);
+  const [allRatingList, setAllRatingList] = useState();
 
   const handlePopup = () => {
     if (popupVisible == false){
@@ -31,6 +36,61 @@ export default function StarRating({ productId, count, icon }) {
     };
     fetchRating();
   }, [productId]);
+
+  useEffect(() => {
+    const makeRatingArray = async () =>{
+      const firestoreRatings = await fetchAllRatings();
+      console.log(firestoreRatings);
+      const firestoreAvRating = ArraySum(firestoreRatings) / firestoreRatings.length;
+      console.log("FSAv is "+firestoreAvRating);
+      const roundedAv = Math.round(firestoreAvRating);
+      if(!isNaN(firestoreAvRating)){
+      setAllRatingList(roundedAv);
+      }
+      else{
+        setAllRatingList(count)
+      }
+    }
+    makeRatingArray();
+  },[]);
+
+  const ArraySum = (numbers) => {
+    console.log(numbers);
+    const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum;
+  }
+
+  let finalFSRatings = [];
+  let finalFSArray = [];
+
+
+  const fetchAllRatings = async () => {
+    console.log("ranRatingFetch")
+    const snapshot = await getDocs(collection(db, 'userRatings'));
+    
+    for(const snap of snapshot.docs){
+
+    // snapshot.forEach (async (element) => {
+    // console.log(element.id);
+    finalFSRatings.push (await findFSRatings(snap.id));
+    finalFSArray = finalFSRatings.filter(x => !isNaN(x))
+    // console.log(finalFSArray);
+  }
+  // console.log(finalFSArray);
+  return finalFSArray;
+  }
+  const findFSRatings = async (docId) => {
+    const getRatingDoc = await getDoc(doc(db, `userRatings`,docId));
+    if (getRatingDoc.exists()){
+      const data = getRatingDoc.data();
+      if (data.productId == productId)
+      // console.log(data.rating);
+      return(data.rating);
+    }
+    // return("doesNotExist");
+  }
+
+
 
   const handleClick = (rating) => {
     setRating(rating);
@@ -61,7 +121,8 @@ export default function StarRating({ productId, count, icon }) {
 
   return (
     <>
-    <button onClick={handlePopup} className="openSubmitBtn">Submit A Rating</button>
+    <h2 className="averageStars">{allRatingList}★</h2>
+    <button onClick={handlePopup} className="openSubmitBtn">Add A Rating</button>
     
 
       {popupVisible && (
